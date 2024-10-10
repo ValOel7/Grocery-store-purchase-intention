53734bc03f88775b51eca41196a8914483e2965f
ValOel7 <142802666+ValOel7@users.noreply.github.com>
2024-10-10 16:50:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 import pandas as pd
 
-st.title("🎈 My new app")
+st.title("Grocery store purchase intention predictor")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
